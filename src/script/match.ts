5fcbeb6d414f3fbe51fadeb7bc14652024bfdaa4
jwyxym <jwyxym@126.com>
2025-08-17 15:50:52 +0800

    Update match.ts

--- src/script/match.ts
+++ src/script/match.ts
@@ -25,20 +25,16 @@ class Match {
         this.round = obj.round;
         this.isThirdPlaceMatch = obj.isThirdPlaceMatch;
         this.status = obj.status;
-        const name1_str = p.find(i => i.id == obj.player1Id)?.name ?? '';
-        const name1 = name1_str.split(/[\+\uFF0B]/) ?? [];
         this.player1 = {
             id : obj.player1Id,
-            name : (name1.length == 2 && !Number.isNaN(name1[0]) && name1[0].length > 3) ? name1[1] : name1_str,
-            qq : (name1.length == 2 && !Number.isNaN(name1[0]) && name1[0].length > 3) ? name1[0] : undefined,
+            name : p.find(i => i.id == obj.player1Id)?.name ?? '',
+            qq : p.find(i => i.id == obj.player1Id)?.qq ?? undefined,
             score : obj.player1Score
         };
-        const name2_str = p.find(i => i.id == obj.player2Id)?.name ?? '';
-        const name2 = name2_str?.split(/[\+\uFF0B]/) ?? [];
         this.player2 = {
             id : obj.player2Id,
-            name : (name2.length == 2 && !Number.isNaN(name2[0]) && name2[0].length > 3) ? name2[1] : name2_str,
-            qq : (name2.length == 2 && !Number.isNaN(name2[0]) && name2[0].length > 3) ? name2[0] : undefined,
+            name : p.find(i => i.id == obj.player2Id)?.name ?? '',
+            qq : p.find(i => i.id == obj.player2Id)?.qq ?? undefined,
             score : obj.player2Score
         };
         this.winnerId = obj.winnerId as number | null;
